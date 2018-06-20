999f0475959220ae98d2b1db6a7bdd6f5ff39480
Mac <570768995@qq.com>
2018-06-20 22:26:08 +0800

    微信对接

--- assets/game/script/managers/gameManager.js
+++ assets/game/script/managers/gameManager.js
@@ -9,6 +9,23 @@ cc.Class({
         dataFunc.loadConfigs();
         clientEvent.on(clientEvent.eventType.gameOver, this.gameOver, this);
         clientEvent.on(clientEvent.eventType.leaveRoomNotify, this.leaveRoom, this);
+        wx.login({
+            success: function() {
+                wx.getUserInfo({
+                    fail: function(res) {
+                        // iOS 和 Android 对于拒绝授权的回调 errMsg 没有统一，需要做一下兼容处理
+                        if (res.errMsg.indexOf('auth deny') > -1 || res.errMsg.indexOf('auth denied') > -1) {
+                            // 处理用户拒绝授权的情况
+                        }
+                    },
+                    success: function(res) {
+                        Game.GameManager.nickName = res.userInfo.nickName;
+                        Game.GameManager.avatarUrl = res.userInfo.avatarUrl;
+                        console.log('success', Game.GameManager.nickName);
+                    }
+                });
+            }
+        })
     },
 
     leaveRoom: function(data) {
