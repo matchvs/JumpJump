3ef91ff18fdbab43cb05e6c1e33343dfba3340c7
Mac <570768995@qq.com>
2018-08-11 16:26:14 +0800

    fix bug

--- assets/game/script/uiGamePanel.js
+++ assets/game/script/uiGamePanel.js
@@ -9,7 +9,7 @@ cc.Class({
             url: cc.AudioClip
         },
         GameRoot: cc.Node,
-        CubeRoot: cc.Node,
+        CubeRoot: cc.Node
     },
     onLoad() {
         this.score = this.node.getChildByName("Board").getChildByName("player1").getChildByName("score");
@@ -19,85 +19,38 @@ cc.Class({
         this.player.setData(GLB.playerUserIds[0]);
         this.rival = this.node.getChildByName("Board").getChildByName("player2").getComponent("resultPlayerIcon");
         this.rival.setData(GLB.playerUserIds[1]);
+
+        clientEvent.on(clientEvent.eventType.leaveRoomMedNotify, this.leaveRoom, this);
     },
-    init(){
+    init() {
         Game.BattleManager.Init();
         this.playBgm();
-        setTimeout(function () {
+        setTimeout(function() {
             Game.BattleManager.InstanceBrickAndPlayer();
             this.m_nTime = 60;
             this.time = setInterval(this.TimeCount.bind(this), 1000);
         }.bind(this), 1000);
     },
-    playBgm(){
+    playBgm() {
         this.audio = cc.audioEngine.play(this.bgmAudio, true, 1);
     },
-    stopBgm(){
+    stopBgm() {
         cc.audioEngine.stop(this.audio);
     },
     exit() {
         uiFunc.openUI("uiExit");
     },
-    updateScore(point){
+    updateScore(point) {
         this.score.getComponent(cc.Label).string = point;
     },
-    updateScore2(point){
+    updateScore2(point) {
         this.score2.getComponent(cc.Label).string = point;
     },
-    leaveRoom(data) {
-        if (Game.GameManager.gameState !== GameState.Over) {
-            uiFunc.openUI("uiTip", function(obj) {
-                var uiTip = obj.getComponent("uiTip");
-                if (uiTip) {
-                    if (data.leaveRoomInfo.userId !== GLB.userInfo.id) {
-                        uiTip.setData("对手离开了游戏");
-                    }
-                }
-            }.bind(this));
-        }
-    },
 
-    rightStart() {
-        this.sendDirectMsg(DirectState.Right);
-    },
-
-    rightCancel() {
-        this.sendDirectMsg(DirectState.None);
-    },
-
-    leftStart() {
-        this.sendDirectMsg(DirectState.Left);
-    },
-
-    leftCancel() {
-        this.sendDirectMsg(DirectState.None);
-
-    },
-
-    sendDirectMsg(direction) {
-        if (Game.GameManager.gameState === GameState.Play) {
-            mvs.engine.sendFrameEvent(JSON.stringify({
-                action: GLB.DIRECTION,
-                direction: direction
-            }));
-        }
-    },
-
-    reloadCartridge() {
-        setTimeout(function() {
-            this.cartridgeBullets = [].concat(this.nodeDict["cartridge"].children);
-            this.bulletCnt = this.cartridgeBullets.length;
-            for (var i = 0; i < this.cartridgeBullets.length; i++) {
-                this.cartridgeBullets[i].getComponent(cc.Animation).play("reloading");
-            }
-            this.lackBulletAnim.stop();
-            this.lackBulletAnim.node.opacity = 0;
-        }.bind(this), 5000);
-    },
     TimeCount() {
         if (Game.GameManager.gameState !== GameState.Over) {
             this.m_nTime -= 1;
-            if(this.node){
+            if (this.node) {
                 this.node.getChildByName("TimeNode").getChildByName("Label").getComponent(cc.Label).string = this.m_nTime
             }
             // cc.log(this.m_nTime);
@@ -106,18 +59,7 @@ cc.Class({
                 clearInterval(this.time);
                 Game.GameManager.gameOver();
             }
-            //GameNetWorkManager_1.GameNetWorkManager.GetInstance().SendGameData({ info: GlobalConfing_1.GlobalConfing.MessageId_Time, time: this.m_nTime });
-            //GameMain_1.default.GetInstance().DlgGameBattle.getComponent(DlgGameBattle_1.default).UpdateTime(this.m_nTime);
         }
-    },
-    exit() {
-        uiFunc.openUI("uiExit");
-    },
-
-
-    roundStart: function() {
-        this.nodeDict['readyGo'].getComponent(cc.Animation).play();
-        this.nodeDict['readyGo'].getComponent(cc.AudioSource).play();
     },
 
     leaveRoom() {
@@ -132,9 +74,6 @@ cc.Class({
     },
 
     onDestroy() {
-        clientEvent.off(clientEvent.eventType.roundStart, this.roundStart, this);
-        clientEvent.off(clientEvent.eventType.gameOver, this.gameOver, this);
-        clientEvent.off(clientEvent.eventType.refreshSlateBtn, this.refreshSlateBtn, this);
         clientEvent.off(clientEvent.eventType.leaveRoomMedNotify, this.leaveRoom, this);
 
     }
